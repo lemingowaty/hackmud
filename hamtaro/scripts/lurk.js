dceed27b13e5cc662a140ed36a3b10e23b4b6ab7
L <lemingowaty@gmail.com>
2019-02-16 17:38:17 +0100

    Reformat

--- hamtaro/scripts/lurk.js
+++ hamtaro/scripts/lurk.js
@@ -1,10 +1,10 @@
 function (C,A){
-	var { t , project , pwd ,q} = A || {},
+	var { t , project , pwd ,q } = A || {},
 	Q = {},
 	result = {
 		nav1 : t.call().split("\n").reverse()[0].split(" | ") ,
 		nav2 : t.call(Q).split("\n").reverse()[0].split(/\s|:"/g) ,
-		[JSON.stringify(q)] : t.call(q)
+		[ JSON.stringify(q) ] : t.call(q)
 	}
 	return result
 }
