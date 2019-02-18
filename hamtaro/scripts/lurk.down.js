82ac82c3ab011c495b33c6104ba4102a11a94e93
L <lemingowaty@gmail.com>
2019-02-18 16:34:02 +0100

    Added ignore
    *.down.js

--- hamtaro/scripts/lurk.down.js
+++
@@ -1,9 +0,0 @@
-function (C,A){
-	var { t , project , pwd ,q} = A || {},
-	Q = {},
-	result = {
-		nav1:t.call().split("\n").reverse()[0].split(" | "),
-		nav2:t.call(Q).split("\n").reverse()[0].split(/(\s|:|")/g)
-	}
-	return result
-}
