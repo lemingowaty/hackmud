48dff78827fda6b8bbd757196e5d18e42b046d4f
L <lemingowaty@gmail.com>
2019-02-24 13:05:42 +0100

    Limitownanie znaków 	modified:   hamtaro/scripts/lurk.js

--- hamtaro/scripts/lurk.js
+++ hamtaro/scripts/lurk.js
@@ -1,31 +1,30 @@
 function( C, A ) {
   var B = #fs.scripts.lib(),
     { T, project, pwd, q } = A || {},
-    calls = [],
-    nav = [
-      T.call().split( "\n" ).reverse()[ 0 ]
-      .split( " | " )
-      ,
-      T.call( {} ).split( "\n" ).reverse()[ 0 ]
-      .split( /\s|:|"/ )
-    ],
     Stfy = O => JSON.stringify( O ),
     CL = ( qry = null ) => {
       let a = T.call( qry )
-      calls.push( {
+      CLog.push( {
         [ Stfy( qry ) ]: a
       } )
       return a
-    }
-  // , crpt = [ 0 , 161 , 162 , 164 , 166 , 167 , 168 , 169 , 170 , 193 , 195 ].map(
-  //     e=>( String.fromCharCode(e) )
-  //   )
+    },
+    CLog = [],
+    crpt = [ 0 , 161 , 162 , 164 , 166 , 167 , 168 , 169 , 170 , 193 , 195 ].map(
+      e=>( String.fromCharCode(e) )
+    ),
+    nav = [
+      CL().split( "\n" ).reverse()[ 0 ]
+      .split( " | " ),
+      CL( {} ).split( "\n" ).reverse()[ 0 ]
+      .split( /\s|:|"/ )
+    ]
 
   nav[ 0 ].pop()
   nav = [
-    nav[ 1 ][ 4 ],
-    ...nav[ 0 ],
-    nav[ 1 ][ 6 ]
+    nav[ 1 ][ 4 ],// Choice
+    ...nav[ 0 ],  // NewsList , PasswordPage
+    nav[ 1 ][ 6 ] // ProjectDir
   ]
 
   CL( {
@@ -42,9 +41,7 @@ function( C, A ) {
     project
   } )
 
-  return [
-    ...calls,
-    nav ,
-    (Date.now()-_ST+"ms")
+  return [ ...calls, nav,
+    ( Date.now() - _ST + "ms" )
   ]
 }
