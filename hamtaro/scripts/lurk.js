31acde37f8846bebfaa1182fad0fe46c6053951a
lemingowaty <lemingowaty@gmail.com>
2019-02-27 13:11:50 +0100

    Proejktowanie metod

--- hamtaro/scripts/lurk.js
+++ hamtaro/scripts/lurk.js
@@ -1,73 +1,71 @@
 function( C, A ) { // hamtaro.lurk { T:#s.cmp.public , project:"name" , pwd:"password" }
   var { T, project, pwd, q } = A || {}
-  ,   crpt = [
+    , crpt = [
         0
-        , 161
-        , 162
-        , 164
-        , 166
-        , 167
-        , 168
-        , 169
-        , 170
-        , 193
-        , 195
+        , 161, 162, 164
+        , 166, 167, 168, 169, 170
+        , 193, 195
       ].map( _ => String.fromCharCode( _ ) )
-  ,   CLog = [ ]
-  ,   $line = "-".repeat(16)
-  ,   outp = [ $line ]
-  ,   Output = _ => outp.push( _ , $line )
-  ,   Stfy = _ => JSON.stringify( _ )
-  ,   CL = ( qry = null ) => {
-        let a = T.call( qry )
-        if (a.ok===false) throw Error("O chuj!" + a.msg)
-        CLog.push( {
-          [ Stfy( qry ) ]: a
-        } )
-        return a
+    , CLog = []
+    , nav = []
+    , $line = "-".repeat( 16 )
+    , outp = [ $line ]
+    , Output = _ => outp.push( _ , $line )
+    , Stfy = _ => JSON.stringify( _ )
+    , CL = ( qry , full ) => {
+        let A
+          , a = T.call( qry )
+        if ( a.ok === false ) throw Error( "O chuj!" + a.msg )
+        A = {
+            a
+          , qry: ( Stfy( qry ) || undefined )
+          , cstr: a.constructor.name
+          , retry: CL.bind( {}, qry )
+        }
+        CLog.push( A )
+        
+        return ( full ? A : a )
       }
-  ,   nav = [
-        CL( )
-        .split( "\n" )
-          .reverse( )[ 0 ]
-            .split( " | " )
-        ,
-        CL( {} )
-        .split( "\n" )
-          .reverse( )[ 0 ]
-            .split( /\s|:|"/ )
-      ]
-
-  nav[ 0 ].pop( )
-  nav = [
-    nav[ 1 ][ 4 ]
-    , ...nav[ 0 ]
-    , nav[ 1 ][ 6 ]
-  ]
-
-  let pick = nav[ 0 ]
-  ,   News = CL.bind( {} , {
-        [ pick ]: nav[ 1 ]
-      } )
-  ,   Abot = CL.bind( {}, {
-        [ pick ]: nav[ 2 ]
-      } )
-  ,   Drct = CL.bind( {}, {
-        [ pick ]: nav[ 3 ]
-        , p: pwd
-        , pass: pwd
-        , password: pwd
-        , project
-      } )
-
-  [
-    // ...CLog,
+  //end var
+  
+  findCmds()
+  let tmp = [
     nav
-    , News( )
-    , Abot( )
-    , Drct( )
-    , ( Date.now( ) - _ST + "ms" )
-  ].forEach(Output)
-
+    , (Date.now() - _ST) + "ms"
+  ]
+  tmp.forEach( Output )
+  
+  
   return outp
+  //----------------------------------------
+
+  function findCmds() {
+    nav = [
+      CL( null, 1)
+       .split( "\n" )
+        .reverse()[ 0 ]
+         // .split( " | " )        
+      ,
+      CL( {}, 1 )
+       .split( "\n" )
+        .reverse()[ 0 ]
+         // .split( /\s|:|"/ )
+    ]
+    #D(nav)
+    
+    // let pick = nav[ 0 ]
+    //   , News = CL.bind( {}, {
+    //         [ pick ]: nav[ 1 ]
+    //   } )
+    //   , Abot = CL.bind( {}, {
+    //         [ pick ]: nav[ 2 ]
+    //   } )
+    //   , Drct = CL.bind( {}, {
+    //         [ pick ]: nav[ 3 ]
+    //     , p: pwd
+    //     , pass: pwd
+    //     , password: pwd
+    //     , project
+    //   } )
+  }
 }
