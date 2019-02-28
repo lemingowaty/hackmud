cbced45323cbc3e6e0d1fd03bee5647ff8cd9318
L <lemingowaty@gmail.com>
2019-02-28 07:16:16 +0100

    Update

--- hamtaro/scripts/lurk.js
+++ hamtaro/scripts/lurk.js
@@ -1,71 +1,54 @@
 function( C, A ) { // hamtaro.lurk { T:#s.cmp.public , project:"name" , pwd:"password" }
   var { T, project, pwd, q } = A || {}
-    , crpt = [
+    , crpt =[
         0
         , 161, 162, 164
         , 166, 167, 168, 169, 170
         , 193, 195
-      ].map( _ => String.fromCharCode( _ ) )
-    , CLog = []
-    , nav = []
+      ]
+    , CLog = [ ]
     , $line = "-".repeat( 16 )
     , outp = [ $line ]
     , Output = _ => outp.push( _ , $line )
     , Stfy = _ => JSON.stringify( _ )
-    , CL = ( qry , full ) => {
-        let A
-          , a = T.call( qry )
-        if ( a.ok === false ) throw Error( "O chuj!" + a.msg )
-        A = {
-            a
-          , qry: ( Stfy( qry ) || undefined )
+    , CL = ( qry, full ) =>{
+        let ans , a = T.call( qry )
+        if ( a.ok===false ) throw Error( a.msg )
+        ans = {
+          a
+          , qry: ( qry ?
+              Stfy( qry )
+              : null )
           , cstr: a.constructor.name
           , retry: CL.bind( {}, qry )
         }
-        CLog.push( A )
-        
-        return ( full ? A : a )
+        CLog.push( ans )
+        return ( full ? ans : a )
       }
-  //end var
-  
-  findCmds()
-  let tmp = [
-    nav
-    , (Date.now() - _ST) + "ms"
-  ]
-  tmp.forEach( Output )
-  
-  
-  return outp
-  //----------------------------------------
+    , nav
+  //
 
-  function findCmds() {
+  FindCmds()
+  Output(nav)
+  Output( Date.now() - _ST + "ms" )
+
+  return outp
+  //------------------------------------------------------------
+  //Functions
+  function FindCmds( ){
     nav = [
-      CL( null, 1)
-       .split( "\n" )
-        .reverse()[ 0 ]
-         // .split( " | " )        
-      ,
-      CL( {}, 1 )
-       .split( "\n" )
-        .reverse()[ 0 ]
-         // .split( /\s|:|"/ )
+        CL( null , 1 )
+      , CL( { } , 1 )
     ]
-    #D(nav)
-    
-    // let pick = nav[ 0 ]
-    //   , News = CL.bind( {}, {
-    //         [ pick ]: nav[ 1 ]
-    //   } )
-    //   , Abot = CL.bind( {}, {
-    //         [ pick ]: nav[ 2 ]
-    //   } )
-    //   , Drct = CL.bind( {}, {
-    //         [ pick ]: nav[ 3 ]
-    //     , p: pwd
-    //     , pass: pwd
-    //     , password: pwd
-    //     , project
-    //   } )
+
+    let charmap = nav.map(
+          e => Array.from(
+              e.a.split( "\n" )
+              .reverse()[ 0 ]
+            )
+          .map( char => char.charCodeAt(0) )
+        )
+    Output(charmap[0])
+    Output(charmap[1])
   }
 }
