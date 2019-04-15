fdbd1b564150e39136b95d1ea98055f932b80f55
lemingowaty <lemingowaty@gmail.com>
2019-04-15 13:29:09 +0200

    Pretty

--- hamtaro/scripts/test.js
+++ hamtaro/scripts/test.js
@@ -1,85 +1,76 @@
 function( CTX, ARG ) {
 //Main{
-const Stfy = _ => JSON.stringify( _ )
-    , crpt = [ 0
-        , 161, 162, 164
-        , 166, 167, 168, 169, 170
-        , 193, 195
-    ]
-    , timer = {
-      _TO,
-      _ST,
-      _END,
-      runtime: _ => ( Date.now( ) - _ST ),
-      left: _ => ( _TO - timer.runtime )
-    }
+    const Jstf = _ => JSON.stringify( _ ),
+        Jprs = _ => JSON.parse( _ ),
+        crpt = [ 0
+            , 161, 162, 164
+            , 166, 167, 168, 169, 170
+            , 193, 195
+        ],
+        timer = {
+            _TO,
+            _ST,
+            _END,
+            runtime: _ => ( Date.now() - _ST ),
+            left: _ => ( _TO - timer.runtime )
+        }
     //
-let l = 0
-    , id = IT()
-    , O = new CLer( ARG.T )
+    let last = 0,
+        id = IT(),
+        O = new CLer( ARG.T )
     //
-
-O.Dial()
-O.Dial( {} )
-
-if (ARG.d) Dbg()
-return O
-
-// }Main
-
+    O.Dial()
+    O.Dial( {} )
+    if ( ARG.d ) Dbg()
+    return O
+// } Main
+//
 // Functions {
-function Dbg(){
-    let line = "-".repeat(16)
-    for (let i of [ CTX , ARG , timer , O] ){
-        #D(i)
-        #D(line)
+function Dbg() {
+    let line = "-".repeat( 16 )
+    for ( let i of [ CTX, ARG, timer, O ] ) {#
+        D( i )# D( line )
     }
 }
+//
 function* IT( x ) {
-    var i = 0
+    var i = x
     while ( 1 ) {
         l = i
         yield i++
     }
 }
-
+    //
 function CLer( tgt ) {
     if ( !tgt ) throw Error( "No Target" )
-    var self = tgt
-        , that = _ => self
-        , { name, call } = tgt
-        , log = []
-        , alog = []
-        , Dial = q => {
-            var q = Stfy( q ) || null
-                , a = call( q )
-                , t = $a.constructor.name
-                , i = id.next()
-                .value
-                , r;
-            r = _ => {
-                _ = call.bind( self, $q )
-                var ta = _()
-                alog.push( ta )
-                log.push( { q: Stfy( q ), a:ta
-                    , i: id.next().value, r, t: ta.constructor.name } )
-                return $ta
+    
+    var self = tgt,
+        that = _ => self,
+        cnter = IT(),
+        { name, call } = tgt
+    //
+    self.log = { q: [], a: [], t: [], full: [], last: null }
+    self.Dial = Q => {
+        let log = self.log
+        var q = JStf( Q ) || null,
+            a = call( q ),
+            t = a.constructor.name,
+            i = cnter.next().value,
+            full = {
+                q, a, t, i,
+                r: ( _ => self.Dial.bind( self, Jprs( q ) ) )()
             }
-            alog.push( $a )
-            log.push( { $q, $a, $r, $t, $i } )
-            return $a
-        }
-    Object.assign( self, {
-        that
-        , log
-        , Dial
-        , alog
-    } )
+        //var
+        for ( let x of "qat" ) log[ x ].push( full[ x ] )
+        log.full( full )
+        log.last = i
+        return full
+    }    
     Object.defineProperty( self, 'self', {
-        value: that()
-        , enumerable: false
-    } )
+        value: that(),
+        enumerable: false
+    } ) // Self Reference
     return self
 }
-// Functions }
+// } Functions
 }
