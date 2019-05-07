01cc4ab17fd9b4724bbb90fad6351c3ee3c8eed7
L <lemingowaty@gmail.com>
2019-05-07 18:44:01 +0200

    Macros + latest test

--- hamtaro/scripts/test.js
+++ hamtaro/scripts/test.js
@@ -1,40 +1,43 @@
 function( CTX, ARG ) {
 //Main{
-    const Jstf = _ => JSON.stringify( _ ),
-        Jprs = _ => JSON.parse( _ )
-    var crpt = String.fromCharCode(
+    const AF = _ => Array.from(_),
+
+        Jstf = _ => JSON.stringify( _ ),
+        Jprs = _ => JSON.parse( _ ),
+        ch2n = _ => _.charCodeAt(0),
+        n2ch = String.fromCharCode
+
+    var CrptNum = [
             0
             , 161, 162
             , 164
             , 166, 167, 168, 169, 170
             , 193
             , 195
-        ),
+        ],
+        CrptStr = n2ch(...CrptNum),
         nl = "\n"
 
     let O = CLer( ARG.T ),
-        DL = O.Dial,
-        Time = Timer(),
-        a1 = DL( ),
-        a2 = DL( { } ),
-        alog = [ a1 , a2 ]
+        alog = [],
+        Time = Timer( O ),
+        DL = _=>{
+            _ = O.Dial(_)
+            alog.push(_)
+            return _
+        }
 
-    if ( ARG.d ) Dbg( ARG.dbg )
+    DL( )
+    DL( { } )
+
     return O
 // } Main
 //-----------|
 // Functions {
-    function Dbg() {
-        let line = "-".repeat( CTX.cols )
-        for ( let v of [ CTX , crpt , Time ,...alog ] ) {
-            #D( v )
-            #D( line )
-        }
-    }
 
     function* IT( x ) {
         let i = 0
-        
+
         while ( 1 ) {
             yield i++
         }
@@ -44,38 +47,40 @@ function( CTX, ARG ) {
         if ( !self ) throw Error( "No Target" )
         let { call } = self,
             cnter = IT()
+
         Object.defineProperties( self, {
+            cnter ,
             self : {
                 get: _ => self,
                 enumerable: false
-            },
+            } ,
             log : {
                 value : {
-                    q: [], a: [], t: [], full: [],
+                //  q: [], a: [], t: [],
+                    full: [],
                     last: null
                 },
                 enumerable : true
             },
-            cnter
         } )
         self.Dial = Q => {
-            let log = self.log
-            var q = Jstf( Q ) || null,
+            let log = self.log,
+                q = Jstf( Q ) || null,
                 a = call( q ),
                 t = a.constructor.name,
                 i = cnter.next().value,
                 full = {
                     q, a, t, i,
-                    r: ( _ => self.Dial.bind( self, Jprs( q ) ) )()
+                    r: self.Dial.bind( self, Jprs(q) )
                 }
-            //var
+            //let
             if ( t == "Object" && a.ok === false )
                 throw Error(a.msg) // Script shifting
             if ( t == "String" )
-            full.strinfo = strOp( a )
+                full.strinfo = strOp( a )
             log.full.push( full )
-            for ( let x of "qat" ) log[ x ].push( full[ x ] )
             log.last = i
+            // for ( let x of "qat" ) log[ x ].push( full[ x ] )
             return full
         }
 
@@ -83,58 +88,62 @@ function( CTX, ARG ) {
     }
 
     function strOp (s) {
-        let nline = 0 ,
-            crp = 0,
-            chMap , divn
-        
-        for (let c of crpt){ 
-            if ( s.includes(c) ) { crp = c ; break }
-        }
-
-        if (c) 
-            { chMap = Array.from(s).map(MapCRPT).join(" ") }
-        
-        if ( s.includes(nl) ){
-            nline = 1
-            divn = s.split("\n")
-        }
-        
-        return { nline , crp , chMap , divn }
-    }
-
-    function MapCRPT (ch) {
-        return {
-            ch ,
-            code : ch.charCodeAt(0),
-            cr : ( _=>{
-                for (let c of crpt) {
-                    if (c === ch) return true
-                    else return false
+        let { length } = s ,
+            nline = s.includes(nl) ,
+            afs = AF(s),
+            crMap = [],
+            chMap = afs.map(
+                (c,i)=>({ i, c, n:ch2n(c) })
+            ),
+            divs = s.split("\n"),
+            div = { divs , crMap:[] }
+            chMap.forEach( _=>{
+                for (let n of CrptNum ) {
+                    if ( _.n === n ) {
+                        crMap.push(_)
+                    }
                 }
-            }) ()
-        }
+            })
+
+            Object.defineProperty( div, "afs", { value : divs.map( p=>AF(p) ) })
+            Object.defineProperty( div, "chMap", {
+                value : div.afs.map(
+                    (D,j)=>D.map(
+                        (c,i)=>({ j , i, c, n:ch2n(c) })
+                    )
+                )
+            })
+            div.chMap.forEach(
+                D => D.forEach(
+                    _ => {
+                        for ( let c of CrptStr ) {
+                            if ( _.c === c ) div.crMap.push(_)
+                        }
+                    }
+                )
+            )
+
+        return Object.defineProperties(
+            { crMap , nline , crMap , div , length  },
+            { chMap:{ value:chMap } , afs:{ value:afs } }
+        )
     }
 
-    function Timer($) {
-        $ = {
-            _TO , _ST, _END,
-            runtime: _ => ( Date.now() - _ST ),
-            left: _ => ( _TO - $.RN )
-        }
-        Object.defineProperties( $, {
-            RN : {
-                get: $.runtime,
-                enumerable: true
-            },
-            TL : {
-                get: $.left,
-                enumerable: true
-            },
-            DN : {
-                get: Date.now,
-                enumerable : true
+    function Timer($={}) {
+        let Omap = { _TO , _ST , _END },
+            gtrs = {
+                RN : _ => ( Date.now() - _ST ),
+                TL : _ => ( _TO - $.RN ) ,
+                DN : Date.now
             }
-        } )
+
+        for ( let p in Omap) Omap[p] = {
+            value:Omap[p] , enumerable:true }
+
+        for ( let p in gtrs ) Omap[p] = {
+            get:gtrs[p] , enumerable:true }
+        // #D(Omap)
+        Object.defineProperties( $ , Omap )
 
         return $
     }
