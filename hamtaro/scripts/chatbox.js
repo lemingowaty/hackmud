4392865498ad4c9c256c739b77cbc742552704e9
L <lemingowaty@gmail.com>
2019-06-10 07:17:28 +0200

    Chatbox

---
+++ hamtaro/scripts/chatbox.js
@@ -0,0 +1,68 @@
+function(C,A){
+    if ( !A ) return `No arguments
+    {
+        c:"channel"
+        to:"username",
+        msg:"String"
+    }
+    To set as default chat script type
+    &hamtaro.chatbox
+    into your chatbox.`
+
+    let
+        SFCC = String.fromCharCode,
+        { c , to , msg } = A,
+        { caller , this_script } = C,
+        DB = #db.f({
+            name:this_script
+        }).first(),
+        last = A.col||DB.users[caller] ,
+        clrs = clr_changer(last)
+        // key =
+        // rgx = /[ ]/g
+
+    msg = msg.split(/(?=\W)/gm)
+        .map( e => `\`${clrs.next().value}${e}\`` )
+        .join("")
+
+    let s = c ? #fs.chats.send({ c , msg })
+        : #fs.chats.tell({ to , msg })
+
+    s = s.ok ? { ok : true , msg : `Message sent to ${c||to}` }
+        : s
+
+    #db.u1( { name:this_script },
+        {
+            $set : {
+               ["users."+caller] : clrs.next().value
+            }
+        }
+    )
+    // #D([
+    //     C , A ,
+    //     #db.f({name:this_script}).first()
+    // ])
+
+    return s
+//} ---------------------------
+function* clr_changer( l="0" ){
+    let clrtab = [..."012345"]
+    for (let i of Range(65,89) )
+        clrtab.push( SFCC(i) )
+    for (let i of Range(97,121) )
+        clrtab.push( SFCC(i) )
+    // #D(clrtab)
+    while (1){
+        for ( let i = clrtab.indexOf(l) ; i<clrtab.length ; i++) yield clrtab[i]
+        l = "0"
+    }
+}
+
+function  Range(st, end){
+    return Array.from(
+            Array(end-st+1).keys() ,
+            (e,i)=>(i+st)
+    )
+}
+
+}
--- hamtaro/scripts/dbg.js
+++ hamtaro/scripts/dbg.js
@@ -1,33 +1,54 @@
 function(C,A){
-let str = Array.from(String.fromCharCode(...[
-        0
-        , 161, 162
-        , 164
-        , 166, 167, 168, 169, 170
-        , 193
-        , 195
-    ]))
-    .map( (ch,i)=>`\`${String(i)[0]}${ch}\`` )
-    .join(""),
-    rgx = /`([0-9A-Za-z])(.+)`/g,
-    charlist = []
-for (let i = 0; i<256; i++)
-    charlist[i] = `${i} ${String.fromCharCode(i)}`
-    
-return [ str , rgx.exec(str) , charlist.join("\n") , String.fromCharCode(0) ]
-// ---------------------------
+//{
+    let
+        { c , to , msg } = A,
+        SFCC = String.fromCharCode,
+        str = Array.from(
+            SFCC(...[
+                0
+                , 161, 162
+                , 164
+                , 166, 167, 168, 169, 170
+                , 193
+                , 195
+                ])
+            ).map( (ch,i)=>`\`${String(i)[0]}${ch}\`` )
+            .join(""),
+        rgx = /`([0-9A-Za-z])(.+)`/g,
+        clrs = clr_changer()
+
+    msg = A.msg.split(/[ ]/g)
+        .map( e => `\`${ clrs.next().value + e }\`` )
+        .join(" ")
+
+    let OUT = c ?
+        #fs.chats.send({ c , msg })
+        : #fs.chats.tell({ to , msg })
+
+
+    return { A , OUT }
+//} ---------------------------
 function* clr_changer(){
-    let clrtab = [..."0123456789"] , i
+    let clrtab = [..."012345"] , i
 
-    // for (i = 65; i<90; i++)
-    //    clrtab.push(String.fromCharCode(i))
+    for ( i of clrtab )
+        yield i
 
-    // for (i = 97; i<=122; i++)
-    //     clrtab.push(String.fromCharCode(i))
+    for ( i of Range(65,89) )
+        yield SFCC(i++)
+
+    for ( i of Range(97,121) )
+        yield SFCC(i++)
+
+    return
 }
 
-function  Range(s, e, inc=1){
+function  Range(st, end){
 
+    return Array.from(
+            Array(end-st+1).keys() ,
+            (e,i)=>(i+st)
+    )
 }
 
 }
